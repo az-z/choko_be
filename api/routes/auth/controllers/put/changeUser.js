7f12e7155b9f2b899ded1ce81dc9d4f100e7ce5b
nevvord <nevvord@gmail.com>
2020-10-17 18:01:10 +0300

    fixes

--- api/routes/auth/controllers/put/changeUser.js
+++ api/routes/auth/controllers/put/changeUser.js
@@ -1,7 +1,7 @@
 module.exports = async (req, res) => {
   const { description, phoneNumber, site, facebook } = req.body
   const currPhoneNumber = phoneNumber
-  const user = await db.Users.findOne({ _id: req.params.id, creator: req.user._id })
+  const user = await db.Users.findOne({ _id: req.user._id })
   user.description = description
   user.facebook = facebook
   user.site = site
