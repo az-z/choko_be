18c251603756c0c69faa32a04df54085c2531774
nevvord <nevvord@gmail.com>
2020-10-17 17:01:51 +0300

    getUSer

--- api/routes/auth/controllers/index.js
+++ api/routes/auth/controllers/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleAuth: require('./post/googleAuth'),
   getUser: require('./get/getUser'),
-  getPublicUser: require('./get/getPublicUSer'),
+  getPublicUser: require('./get/getPublicUser'),
   changeUser: require('./put/changeUser'),
   authAdmin: require('./post/authAdmin'),
   verifyAdmin: require('./get/verifyAdmin')
