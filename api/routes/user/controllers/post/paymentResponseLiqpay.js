819526459026979c5425da46bd04cc0f29529b0d
nevvord <nevvord@gmail.com>
2020-12-06 21:07:43 +0200

    qury

--- api/routes/user/controllers/post/paymentResponseLiqpay.js
+++ api/routes/user/controllers/post/paymentResponseLiqpay.js
@@ -1,7 +1,8 @@
 module.exports = async (req, res) => {
   try {
     console.log("BODY: ", req.body)
-    console.log("HEADER: ", req.header)
+    console.log("HEADER: ", req.headers)
+    console.log("QUERY: ", req.query)
   } catch (error) {
     console.error(error)
   }
