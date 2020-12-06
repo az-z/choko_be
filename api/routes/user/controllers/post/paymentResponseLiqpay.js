0ccd7a0fd20ef70bf0714a5b4227dacdbf82b331
nevvord <nevvord@gmail.com>
2020-12-06 21:40:49 +0200

    data

--- api/routes/user/controllers/post/paymentResponseLiqpay.js
+++ api/routes/user/controllers/post/paymentResponseLiqpay.js
@@ -4,6 +4,7 @@ module.exports = async (req, res) => {
     console.log("HEADER: ", req.headers)
     console.log("QUERY: ", req.query)
     console.log("order_id: ", req.order_id)
+    console.log("DATA: ", req.data)
   } catch (error) {
     console.error(error)
   }
--- api/routes/user/router.js
+++ api/routes/user/router.js
@@ -3,5 +3,5 @@ const controllers = require('./controllers')
 router
   .get('/statuses/getpayments', middleware.verify.default, controllers.getHtmlPayments)
   .post('/responseliqpay', controllers.paymentResponseLiqpay)
-
+  
 module.exports = router
