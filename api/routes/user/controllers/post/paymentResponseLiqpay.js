189eb9ed27b586a83f3d36ba6e87c715c484a105
nevvord <nevvord@gmail.com>
2020-12-13 15:52:37 +0200

    dates

--- api/routes/user/controllers/post/paymentResponseLiqpay.js
+++ api/routes/user/controllers/post/paymentResponseLiqpay.js
@@ -5,10 +5,10 @@ module.exports = async (req, res) => {
     const payment = await db.Payments.findOne({ _id: data.order_id })
     const user = await db.Users.findOne({ _id: payment.user })
     const currUserDate = await new Date(user.active.to)
-    const date = new Date()
+    const date = await new Date()
     if ( data.description === 'month' ) {
       const dateMonth = new Date(date.setMonth(date.getMonth()+1))
-      user.active.to = currUserDate < date ? dateMonth : new Date(currUserDate.setMonth(currUserDate.getMonth() + 1))
+      user.active.to = currUserDate <= date ? dateMonth : new Date(currUserDate.setMonth(currUserDate.getMonth() + 1))
       user.active.status = true
       user.active.trial = false
       payment.status = true
@@ -17,7 +17,7 @@ module.exports = async (req, res) => {
     }
     if ( data.description === 'year' ) {
       const dateYear = new Date(date.setFullYear(date.getFullYear() + 1))
-      user.active.to = currUserDate < date ? dateYear : new Date(currUserDate.setFullYear(currUserDate.getFullYear() + 1))
+      user.active.to = currUserDate <= date ? dateYear : new Date(currUserDate.setFullYear(currUserDate.getFullYear() + 1))
       user.active.trial = false
       user.active.status = true
       payment.status = true
