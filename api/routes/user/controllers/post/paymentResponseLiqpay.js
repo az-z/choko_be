8c56b39767453fd2f4f07d7fff7500259d9499f8
nevvord <nevvord@gmail.com>
2020-12-13 17:42:30 +0200

    dates

--- api/routes/user/controllers/post/paymentResponseLiqpay.js
+++ api/routes/user/controllers/post/paymentResponseLiqpay.js
@@ -7,11 +7,12 @@ module.exports = async (req, res) => {
     console.log('USER: ', user.active.to);
     const currUserDate = new Date(user.active.to)
     console.log('CurrUserDate: ', currUserDate);
-    const date = new Date()
+    const currentDate = new Date()
     if ( data.description === 'month' && payment.status === false ) {
+      const date = new Date()
       // console.log('New date: ', currUserDate > date ? new Date(currUserDate.setMonth(currUserDate.getMonth() + 1)) : dateMonth);
       const dateMonth = new Date(date.setMonth(date.getMonth() +1 ))
-      user.active.to = currUserDate > date ? new Date(currUserDate.setMonth(currUserDate.getMonth() + 1)) : dateMonth
+      user.active.to = currUserDate > currentDate ? new Date(currUserDate.setMonth(currUserDate.getMonth() + 1)) : dateMonth
       user.active.status = true
       user.active.trial = false
       payment.status = true
@@ -20,9 +21,10 @@ module.exports = async (req, res) => {
       console.log("Saved month", saveUser);
     }
     if ( data.description === 'year' && payment.status === false ) {
+      const date = new Date()
       const dateYear = new Date(date.setFullYear(date.getFullYear() + 1))
       console.log('DEB; ', currUserDate.getFullYear(), date.getFullYear(), currUserDate.getFullYear() > date.getFullYear());
-      user.active.to = currUserDate.getFullYear() > date.getFullYear() ? new Date(currUserDate.setFullYear(currUserDate.getFullYear() + 1)) : dateYear
+      user.active.to = currUserDate > currentDate ? new Date(currUserDate.setFullYear(currUserDate.getFullYear() + 1)) : dateYear
       // console.log('Year + 1: ', currUserDate > date ? new Date(currUserDate.setFullYear(currUserDate.getFullYear() + 1)) : dateYear, '|', new Date(currUserDate.setFullYear(currUserDate.getFullYear() + 1)))
       user.active.trial = false
       user.active.status = true
