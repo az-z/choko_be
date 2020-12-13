fbfe5da77b75ed1b25fbd4c2adc75a14ae34aa4f
nevvord <nevvord@gmail.com>
2020-12-13 15:05:55 +0200

    year

--- api/routes/user/controllers/post/paymentResponseLiqpay.js
+++ api/routes/user/controllers/post/paymentResponseLiqpay.js
@@ -3,27 +3,22 @@ module.exports = async (req, res) => {
   try {
     const data = JSON.parse(base64decode(req.body.data))
     const payment = await db.Payments.findOne({ _id: data.order_id })
-    // console.log(payment)
     const user = await db.Users.findOne({ _id: payment.user })
     const currUserDate = new Date(user.active.to)
+    const date = new Date()
     if ( data.description === 'month' ) {
-      const date = new Date()
       const dateMonth = new Date(date.setMonth(date.getMonth()+1))
-      user.active.to = currUserDate < date ? dateMonth : new Date(currUserDate.setMonth(currUserDate.getMonth()+1))
+      user.active.to = currUserDate < date ? dateMonth : new Date(currUserDate.setMonth(currUserDate.getMonth() + 1))
       user.active.status = true
       payment.status = true
-      // console.log('user :', user);
       const paymentSave = payment.save()
       const saveUser = await user.save()
     }
     if ( data.description === 'year' ) {
       payment.status = true
-      const date = new Date()
+      const dateYear = new Date(date.setFullYear(date.getFullYear() + 1))
       user.active.status = true
-      // user.active.to = date.setFullYear(date.getFullYear() + 1)
-      user.active.to = new Date(user.active.to) < date ? new Date(date.setFullYear(date.getFullYear() + 1)) : new Date(user.active.to) + new Date(date.setFullYear(date.getFullYear() + 1))
-      
-      // console.log('user :', user);
+      user.active.to = currUserDate < date ? dateYear : new Date(currUserDate.setFullYear(currUserDate.getFullYear() + 1))
       const paymentSave = payment.save()
       const saveUser = await user.save()
     }
