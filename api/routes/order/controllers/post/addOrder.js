a61e670c680bbbbd0cfef3e46b82f1f58bea4e8b
Andrew Z <formisc@gmail.com>
2021-01-14 14:22:33 -0500

    working new cash order

--- api/routes/order/controllers/post/addOrder.js
+++ api/routes/order/controllers/post/addOrder.js
@@ -6,33 +6,29 @@ module.exports = async (req, res) => {
   if (!user) return res.status(400).send({ msg: 'Пользователь отсутствует' })
   // if (!name) return res.status(400).send({ msg: 'Имя обязательно' })
   if (!email) return res.status(400).send({ msg: 'E-mail обязателен' })
+
+  const currGallery = await db.Galleries.findOne({ _id: gallery })
   const currUser = await db.Users.findOne({ _id: user })
-  const currGallery = await db.Galleries.find({ _id: gallery })
+
   const order = new db.Orders({
     _id: new Types.ObjectId(),
     user, images, gallery, name, lastname, phone, email, summ
   })
   currUser.orders.push(order.id)
-  const orderResultat = await order.save()
-  if (!orderResultat) return res.status(500).send({ msg: 'Неудалось сохранить заказ в базу данных' })
-  const userResultat = await currUser.save()
-  if (!userResultat) {
-    console.log(userResultat)
+  const orderResult = await order.save()
+  if (!orderResult) return res.status(500).send({ msg: 'Неудалось сохранить заказ в базу данных' })
+
+  const userResult = await currUser.save()
+  if (!userResult) {
+    console.log(userResult)
     return res.status(500).send({ msg: 'Неудалось обновить пользователя' })
   }
-  // const message = {
-  //   to: email,
-  //   subject: 'Заказ оформлен успешно',
-  //   text: 'Спасибо за заказ, после оплаты вам прийдет письмо с сылкой на ваши фото!'
-  // }
-  // mailer(message)
 
   // currUser.payment.cashText - Текст заполненый пользователем для оплаты наличными
   // currGallery - есть вся инфа с модели /db/models/Galleries.js
   // currGallery.payment - имеет типы cash(наличными) и liqpay(оплата картой)
   // if (currGallery.payment === 'cash') return;
   // const typeValue = currGallery.payment === 'cash' ? currUser.payment.cashText : ''
-
 
   const payload = {
     "To": [
@@ -54,4 +50,5 @@ module.exports = async (req, res) => {
 
   mailer(payload)
   res.send({ msg: 'Заказ создан успешно', order })
+
 }
