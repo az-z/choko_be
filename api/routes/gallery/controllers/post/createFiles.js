dc956fc4ff7bb7bcd4236672683cb1865c9ffbc2
nevvord <nevvord@gmail.com>
2021-01-17 21:49:52 +0200

    remove trycatch

--- api/routes/gallery/controllers/post/createFiles.js
+++ api/routes/gallery/controllers/post/createFiles.js
@@ -45,56 +45,51 @@ module.exports = async (req, res) => {
   const { gallery } = req.body
   if (!images) return res.status(404).send({ msg: "Изображений ненайденно" })
   if (req.sorageLimitError) return res.status(500).send({ msg: "Недостаточно места на диске" })
-  try {
-    let savedImages = []
-    const pathToOriginalFile = `uploads/${req.user._id}/${gallery}/${images[0].filename}`
-    const savePath = `uploads/${req.user._id}/${gallery}`
-    const newImage = new db.Images({
-      _id: new Types.ObjectId(),
-      name: images[0].filename,
-      originalName: images[0].originalname,
-      path: {
-        original: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/${images[0].filename}`,
-        small: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/small_${images[0].filename}`,
-        xs: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/xs_${images[0].filename}`
-      },
-      size: images[0].size,
-      uploader: req.user._id,
-      gallery
-    })
-    req.user.storage.usage += images[0].size
-    if (req.user.storage.usage >= req.user.storage.limit) req.user.storage.full = true
-    else req.user.storage.full = false
-    savedImages.push(newImage._id)
-    // const exifData = await exif(pathToOriginalFile)
-    // const exifData = await exif(pathToOriginalFile).catch(error => console.error(error))
-    let exifData = null
-    new ExifImage({ image: pathToOriginalFile }, function (error, data) {
-      if (error)
-        console.log('Error: ' + error.message)
-      else
-        exifData = data
-    })
-    const watermark = await Jimp.read('static/watermark.png')
-    console.log('watermark read done', true);
-    const payload = {
-      path: pathToOriginalFile,
-      watermark,
-      exif: exifData,
-      scale: .3,
-      type: 'small',
-      savePath,
-      quality: 80,
-      filename: images[0].filename
-    }
-    const resultatResizeWatermark = await resize(payload)
-    const resultatResizeXS = await resize({ ...payload, type: 'xs', scale: .1, watermark: null, qualit: 50 })
-    const saveImage = await newImage.save()
-    req.user.images = req.user.images.concat(savedImages)
-    const saveUserResultat = await req.user.save()
-    return res.status(200).send({ msg: 'Фото загружены успешно', images: savedImages })
-  } catch (error) {
-    console.error(error)
-    res.status(500).send({ msg: "Ошибка сервера", error })
+  let savedImages = []
+  const pathToOriginalFile = `uploads/${req.user._id}/${gallery}/${images[0].filename}`
+  const savePath = `uploads/${req.user._id}/${gallery}`
+  const newImage = new db.Images({
+    _id: new Types.ObjectId(),
+    name: images[0].filename,
+    originalName: images[0].originalname,
+    path: {
+      original: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/${images[0].filename}`,
+      small: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/small_${images[0].filename}`,
+      xs: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/xs_${images[0].filename}`
+    },
+    size: images[0].size,
+    uploader: req.user._id,
+    gallery
+  })
+  req.user.storage.usage += images[0].size
+  if (req.user.storage.usage >= req.user.storage.limit) req.user.storage.full = true
+  else req.user.storage.full = false
+  savedImages.push(newImage._id)
+  // const exifData = await exif(pathToOriginalFile)
+  // const exifData = await exif(pathToOriginalFile).catch(error => console.error(error))
+  let exifData = null
+  new ExifImage({ image: pathToOriginalFile }, function (error, data) {
+    if (error)
+      console.log('Error: ' + error.message)
+    else
+      exifData = data
+  })
+  const watermark = await Jimp.read('static/watermark.png')
+  console.log('watermark read done', true);
+  const payload = {
+    path: pathToOriginalFile,
+    watermark,
+    exif: exifData,
+    scale: .3,
+    type: 'small',
+    savePath,
+    quality: 80,
+    filename: images[0].filename
   }
+  const resultatResizeWatermark = await resize(payload)
+  const resultatResizeXS = await resize({ ...payload, type: 'xs', scale: .1, watermark: null, qualit: 50 })
+  const saveImage = await newImage.save()
+  req.user.images = req.user.images.concat(savedImages)
+  const saveUserResultat = await req.user.save()
+  return res.status(200).send({ msg: 'Фото загружены успешно', images: savedImages })
 }
