c4a02d141827e68d4f4c343796857cb1ae4f1aca
nevvord <nevvord@gmail.com>
2021-01-17 23:53:08 +0200

    xs return

--- api/routes/gallery/controllers/get/getGallery.js
+++ api/routes/gallery/controllers/get/getGallery.js
@@ -1,7 +1,7 @@
 module.exports = async (req, res) => {
   const { id } = req.params
   const gallery = await db.Galleries.findOne({ _id: id, creator: req.user._id })
-    .populate([{ path: 'images', select: 'path.small originalName' }, 'creator'])
+    .populate([{ path: 'images', select: 'path.xs originalName' }, 'creator'])
   if (!gallery) return res.status(404).send({ msg: 'Галлерея с таким id не найденна' })
   res.send({ msg: 'Галерея найденна', gallery })
 }
