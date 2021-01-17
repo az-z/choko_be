fefa1d982dfa4adcf83fb4c8957d261723339d7f
nevvord <nevvord@gmail.com>
2021-01-17 19:03:00 +0200

    comment all try

--- api/routes/gallery/controllers/post/createFiles.js
+++ api/routes/gallery/controllers/post/createFiles.js
@@ -45,52 +45,52 @@ module.exports = async (req, res) => {
   if (!images) return res.status(404).send({ msg: "Изображений ненайденно" })
   if (req.sorageLimitError) return res.status(500).send({ msg: "Недостаточно места на диске" })
   try {
-    let savedImages = []
-    const pathToOriginalFile = `uploads/${req.user._id}/${gallery}/${images[0].filename}`
-    const savePath = `uploads/${req.user._id}/${gallery}`
-    const newImage = new db.Images({
-      _id: new Types.ObjectId(),
-      name: images[0].filename,
-      originalName: images[0].originalname,
-      path: {
-        original: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/${images[0].filename}`,
-        small: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/small_${images[0].filename}`,
-        xs: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/xs_${images[0].filename}`
-      },
-      size: images[0].size,
-      uploader: req.user._id,
-      gallery
-    })
-    req.user.storage.usage += images[0].size
-    if (req.user.storage.usage >= req.user.storage.limit) req.user.storage.full = true
-    else req.user.storage.full = false
-    savedImages.push(newImage._id)
+    // let savedImages = []
+    // const pathToOriginalFile = `uploads/${req.user._id}/${gallery}/${images[0].filename}`
+    // const savePath = `uploads/${req.user._id}/${gallery}`
+    // const newImage = new db.Images({
+    //   _id: new Types.ObjectId(),
+    //   name: images[0].filename,
+    //   originalName: images[0].originalname,
+    //   path: {
+    //     original: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/${images[0].filename}`,
+    //     small: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/small_${images[0].filename}`,
+    //     xs: `${process.env.FULL_PATH}/${req.user._id}/${gallery}/xs_${images[0].filename}`
+    //   },
+    //   size: images[0].size,
+    //   uploader: req.user._id,
+    //   gallery
+    // })
+    // req.user.storage.usage += images[0].size
+    // if (req.user.storage.usage >= req.user.storage.limit) req.user.storage.full = true
+    // else req.user.storage.full = false
+    // savedImages.push(newImage._id)
     // const exifData = await exif(pathToOriginalFile).catch(error => console.error(error))
     // const watermark = await Jimp.read('static/watermark.png')
     // console.log('watermark read done', true);
-    const payload = {
-      path: pathToOriginalFile,
-      watermark: null,
-      exif: null,
-      // exif: exifData,
-      scale: .3,
-      type: 'small',
-      savePath,
-      quality: 80,
-      filename: images[0].filename
-    }
-    const resultatResizeWatermark = await resize(payload)
-    console.log('resultatResizeWatermark', true);
-    const resultatResizeXS = await resize({ ...payload, type: 'xs', scale: .1, watermark: null, qualit: 50 })
-    console.log('resultatResizeXS', true);
-    const saveImage = await newImage.save()
-    console.log('saveImage', true, saveImage);
-    req.user.images = req.user.images.concat(savedImages)
-    console.log('saveImage', true);
-    const saveUserResultat = await req.user.save()
-    console.log('saveUserResultat', true);
-    res.status(200).json({ msg: 'Фото загружены успешно', images: savedImages })
-    console.log('after response log!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    // const payload = {
+    //   path: pathToOriginalFile,
+    //   watermark: null,
+    //   exif: null,
+    //   // exif: exifData,
+    //   scale: .3,
+    //   type: 'small',
+    //   savePath,
+    //   quality: 80,
+    //   filename: images[0].filename
+    // }
+    // const resultatResizeWatermark = await resize(payload)
+    // console.log('resultatResizeWatermark', true);
+    // const resultatResizeXS = await resize({ ...payload, type: 'xs', scale: .1, watermark: null, qualit: 50 })
+    // console.log('resultatResizeXS', true);
+    // const saveImage = await newImage.save()
+    // console.log('saveImage', true, saveImage);
+    // req.user.images = req.user.images.concat(savedImages)
+    // console.log('saveImage', true);
+    // const saveUserResultat = await req.user.save()
+    // console.log('saveUserResultat', true);
+    // res.status(200).json({ msg: 'Фото загружены успешно', images: savedImages })
+    // console.log('after response log!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
   } catch (error) {
     console.error(error)
     res.status(500).send({ msg: "Ошибка сервера", error })
